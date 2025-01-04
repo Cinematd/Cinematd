6c311007a604898fcb7755af0533bec32db73e90
Pride <20098992+PrideInt@users.noreply.github.com>
2025-01-04 09:25:28 -0500

    Routing changes to support GitHub Pages; temporary

--- movieapp/src/index.jsx
+++ movieapp/src/index.jsx
@@ -5,16 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <AuthContextProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
 
-    </BrowserRouter>
+    </HashRouter>
   </AuthContextProvider>
 );
